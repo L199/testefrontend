de7e88d19d60eaf18574ab473675c263026003b7
L199 <lucas.ferreira961245@gmail.com>
2024-05-14 17:21:34 -0300

    aprendednoooooooooo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# testefrontend
+# My Project
+
+Este é um exemplo de um projeto npm com um frontend simples em React.
+
+## Estrutura do Projeto
+
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Olá, Mundo!</h1>
+            <p>Este é um exemplo de aplicação React.</p>
+        </div>
+    );
+}
+
+export default App;
